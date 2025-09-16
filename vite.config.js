3573738edab57fb1dda8a108b4488cf89d6715a1
Fayaz Ahamed D <54717833+fayazahamedd@users.noreply.github.com>
2025-09-16 23:16:30 +0530

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,34 @@
 /* eslint-disable no-undef */
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
-import path from 'path';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import path from "path";
 
 // Define paths for easier imports
-const pathResolve = dir => path.resolve(__dirname, dir);
+const pathResolve = (dir) => path.resolve(__dirname, dir);
 
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      '@component': pathResolve('./src/Components'),
-      '@pages': pathResolve('./src/pages'),
+      "@component": pathResolve("./src/Components"),
+      "@pages": pathResolve("./src/pages"),
     },
   },
-  
+  build: {
+    outDir: "build",
+    sourcemap: false,
+    target: "es2018",
+    brotliSize: false,
+    chunkSizeWarningLimit: 2000,
+    rollupOptions: {
+      output: {
+        mimeTypes: {
+          "application/javascript": ["js", "jsx"],
+        },
+        scriptType: "module",
+      },
+    },
+  },
   server: {
     port: 3210,
     open: true, // Open the default browser when the server starts
@@ -22,10 +36,10 @@ export default defineConfig({
 
   optimizeDeps: {
     include: [
-      '@mui/material',
-      '@mui/x-date-pickers',
-      '@mui/lab',
-      '@mui/icons-material',
+      "@mui/material",
+      "@mui/x-date-pickers",
+      "@mui/lab",
+      "@mui/icons-material",
     ],
   },
 
@@ -33,7 +47,7 @@ export default defineConfig({
     // Disable source maps for production build
     sourcemap: false,
     // Set the target environment to ECMAScript 2018
-    target: 'es2018',
+    target: "es2018",
     // Disable Brotli size display in the build summary
     brotliSize: false,
     // Set the warning limit for chunk sizes during build
@@ -42,10 +56,10 @@ export default defineConfig({
       output: {
         // Define MIME types for JavaScript files with 'js' and 'jsx' extensions
         mimeTypes: {
-          'application/javascript': ['js', 'jsx']
+          "application/javascript": ["js", "jsx"],
         },
         // Set the script type to 'module'
-        scriptType: 'module',
+        scriptType: "module",
       },
     },
   },
